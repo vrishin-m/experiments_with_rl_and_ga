fe09990283a6d23ab1281bb6172b8ec74f0433d3
vrishin-m <vrishinmahadevan@gmail.com>
2026-06-03 08:34:16 +0530

    feat: made track and checkpoints

--- cars/python_stuff/comms.py
+++ cars/python_stuff/comms.py
@@ -1,39 +1,50 @@
 import socket
 import json
+import time 
+import neural_network
 
 
 ip = "127.0.0.1"
 port = 4242
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+max_frames = 1000
+
+
+time.sleep(1) 
 
 
 client_socket.sendto("INIT".encode(), (ip, port))
-print("server running")
+print("Python JSON worker running...")
 
+try:
+    while True:
+        try:
+            data, sender_address = client_socket.recvfrom(1024)
+            decoded_msg = data.decode('utf-8')
+            
+            if decoded_msg == "INIT":
+                continue
+                
+            godot_json = json.loads(decoded_msg)
+            python_response = {0: [1,0]}
+            #python_response = neural_network.final_result(decoded_msg)
+            print(decoded_msg)
+            if decoded_msg["0"][2] >= max_frames:
+                break
+            json_string = json.dumps(python_response)
+            client_socket.sendto(json_string.encode('utf-8'), sender_address)
 
-while True:
-    data, sender_address = client_socket.recvfrom(1024)
-    decoded_msg = data.decode('utf-8')
-    
-        
-    try:
+        except ConnectionResetError:
 
-        godot_json = json.loads(decoded_msg)
-        godot_fps = godot_json.get("fps", 0)
+            print("godot not available")
+            time.sleep(0.5)
+            client_socket.sendto("INIT".encode(), (ip, port)) 
+            
+        except json.JSONDecodeError:
+            print("invalid data from godot")
+            continue
 
-        
-    except json.JSONDecodeError:
-        print("invalid json")
-        continue
-
-    python_response = {
-        0: [3,0],
-        1: [5,0.1],
-        2: [0,0.5]
- 
-
-    }
-    
-    json_string = json.dumps(python_response)
-    client_socket.sendto(json_string.encode('utf-8'), sender_address)
-
+except KeyboardInterrupt:
+    print("\nshutting down python client")
+finally:
+    client_socket.close()
--- cars/python_stuff/neural_network.py
+++ cars/python_stuff/neural_network.py
@@ -1,5 +1,15 @@
 import torch
 
+max_acc=1
+max_angle =0.05
+
+def final_result(data):
+    pass
 
 
+def fitness(data):
+    return (data[0]**4)*100 - data[1]**2 - data[2] 
 
+#separate neural networks for each tank. weights are updated every frame with gradient descent. so every frame 
+#there is a forward pass and a backward pass. then, at the end of each iteration, we breed the top performers so
+#that the weights are updated by genetics as well
